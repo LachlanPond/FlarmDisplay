017dd24c48f7c148f8d33e0778b8215f08363c94
Lachlan Pond <lachipond@gmail.com>
2022-05-24 22:04:11 +1000

    Completed basic PFLAA test script

--- Software/NMEA Tester/PFLAA_multi_orbit.py
+++ Software/NMEA Tester/PFLAA_multi_orbit.py
@@ -3,6 +3,7 @@ import serial
 import time
 import pynmea2
 import math
+import random
 from lib import *
 
 #ser = serial.Serial('/dev/ttyS1', 9600, timeout=5.0)
@@ -15,11 +16,15 @@ from lib import *
 
 def orbit(alarm):
     for i in range(12):
-        relNorth = str(math.floor(1000 * math.sin(math.radians(i*30))))
-        relEast = str(math.floor(1000 * math.cos(math.radians(i*30))))
-        relVert = str(-600 + i*100)
-        msg = pynmea2.GGA('PF', 'LAA', (alarm, relNorth, relEast, relVert))
-        sendNMEA(msg)
-        time.sleep(0.5)
+        for y in range(3):
+            relNorth = str(math.floor(1000 * math.sin(math.radians(i*30 + y*60))))
+            relEast = str(math.floor(1000 * math.cos(math.radians(i*30 + y*60))))
+            relVert = str(-800 + i*100 + y*100)
+            msg = pynmea2.GGA('PF', 'LAA', (alarm, relNorth, relEast, relVert, '0', '5A77B' + str(y), '0', '', '112', '5.2', '1'))
+            sendNMEA(msg)
+            time.sleep(random.randrange(3,10)/100)
 
 orbit('0')
+orbit('1')
+orbit('2')
+orbit('3')
